99e851ab5e8ba7a100cf5c641842faccae6111a8
Eominique <slawex.on@interia.pl>
2022-02-07 20:13:48 +0100

    ListAdapter with binding

--- app/src/main/java/com/example/todo/ui/MainActivity.kt
+++ app/src/main/java/com/example/todo/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.todo
+package com.example.todo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todo.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
--- app/src/main/java/com/example/todo/ui/task/TasksViewModel.kt
+++ app/src/main/java/com/example/todo/ui/task/TasksViewModel.kt
@@ -1,10 +1,14 @@
-package com.example.todo.ui
+package com.example.todo.ui.task
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.example.todo.data.TaskDao
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-
+@HiltViewModel
 class TasksViewModel @Inject constructor(
     private val taskDao: TaskDao
 ): ViewModel() {
+
+    val tasks = taskDao.getTasks().asLiveData()
 }
