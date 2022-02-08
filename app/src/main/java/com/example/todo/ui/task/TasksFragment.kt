f8ff196fc7663b4d1d29a1ef7d8c8ea58126a2a7
Eominique <slawex.on@interia.pl>
2022-02-08 21:11:04 +0100

    Combining multiple Flows

--- app/src/main/java/com/example/todo/ui/task/TasksFragment.kt
+++ app/src/main/java/com/example/todo/ui/task/TasksFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todo.R
+import com.example.todo.data.SortOrder
 import com.example.todo.databinding.FragmentTasksBinding
 import com.example.todo.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
@@ -54,18 +55,18 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId){
+        return when (item.itemId) {
             R.id.action_sort_by_name -> {
-
+                viewModel.sortOrder.value = SortOrder.BY_NAME
                 true
             }
             R.id.action_sort_by_date_created -> {
-
+                viewModel.sortOrder.value = SortOrder.BY_DATE
                 true
             }
             R.id.action_hide_completed_tasks -> {
                 item.isChecked = !item.isChecked
-
+                viewModel.hideCompleted.value = item.isChecked
                 true
             }
             R.id.action_delete_all_completed_tasks -> {
